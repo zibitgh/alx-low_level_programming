e1488af5139f96d382eb62668b9d3995839508b6
Richard Entsie <richardentsie88@gmail.com>
2023-02-28 08:16:53 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Succcess)
+ */
+int main(void)
+{
+	fput("and that oiece of art is useful\" -Dora Korpar, 2015-10-19\n",stdout);
+return (1);
+}
