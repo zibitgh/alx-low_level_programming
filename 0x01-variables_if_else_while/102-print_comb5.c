464767b9d38b94f73017099bde2e2f55013e9a23
Richard Entsie <richardentsie88@gmail.com>
2023-03-01 06:26:12 -0800

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,7 +26,7 @@ for (tho = 0 ; tho < 10 ; tho++)
 				putchar(32);
 				putchar('0' + ten);
 				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
+				if (!(tho == 9 && hun == 9))
 				{
 					putchar(',');
 					putchar(32);
